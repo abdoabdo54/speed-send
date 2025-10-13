dcaa37823f731fb97588a76c3ee1f08e8acd7cca
wieckdavid4-alt <wieckdavid4@gmail.com>
2025-10-13 13:56:33 +0100

    Cleanup: remove legacy deploy/fix/diagnostic scripts; single DEPLOY.sh remains

--- add_test_after_columns.sql
+++
@@ -1,6 +0,0 @@
--- Add test_after_email and test_after_count columns to campaigns table
-ALTER TABLE campaigns ADD COLUMN test_after_email VARCHAR(255);
-ALTER TABLE campaigns ADD COLUMN test_after_count INTEGER DEFAULT 0;
-
--- Update existing campaigns to have default values
-UPDATE campaigns SET test_after_count = 0 WHERE test_after_count IS NULL;
